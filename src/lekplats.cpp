c823c4f7af82ddfccc08e121aaf147fb34adaac3
A <alexandre.tussiot@gmail.com>
2018-05-06 15:03:39 +0300

    Add a third cluster to example

--- src/lekplats.cpp
+++ src/lekplats.cpp
@@ -9,23 +9,27 @@
 
 int main(int argc, char* argv[])
 {
-    ClusterDescription descr1;
-    descr1.mean = Point2D{ 240.0, 350.2 };
-    descr1.std_dev = Point2D{ 71.4, 15.7 };
-    descr1.points_count = 800;
+    ClusterDescription d1, d2, d3;
 
-    ClusterDescription descr2;
-    descr2.mean = Point2D{ 354.0, 270.4 };
-    descr2.std_dev = Point2D{ 20.4, 90.7 };
-    descr2.points_count = 600;
+    d1.mean = Point2D{ 240.0, 350.2 };
+    d1.std_dev = Point2D{ 71.4, 15.7 };
+    d1.points_count = 800;
 
-    const std::vector<ClusterDescription> info = { descr1, descr2 };
+    d2.mean = Point2D{ 354.0, 270.4 };
+    d2.std_dev = Point2D{ 20.4, 90.7 };
+    d2.points_count = 600;
+
+    d3.mean = Point2D{ 201.3, 194.4 };
+    d3.std_dev = Point2D{ 27.8, 28.7 };
+    d3.points_count = 700;
+
+    const std::vector<ClusterDescription> info = { d1, d2, d3 };
     ClusterGenerator generator{ info };
 
     const auto& originalClusters = generator.getClusters();
     const auto data = generator.getMergedClusters();
 
-    const auto means = kmeans(data, 2);
+    const auto means = kmeans(data, 3);
     const auto kmeansClusters = makeClusters(data, means);
 
     if (!saveToFile("original.png", originalClusters))
@@ -39,7 +43,7 @@ int main(int argc, char* argv[])
         std::cerr << "Could not save second image" << std::endl;
     }
 
-    for (size_t i = 0; i < 2; ++i)
+    for (size_t i = 0; i < means.size(); ++i)
     {
         std::cout << "Mean: " << means[i].x << " - " << means[i].y << std::endl;
         std::cout << "Points in the cluster: " << kmeansClusters[i].size() << std::endl;
